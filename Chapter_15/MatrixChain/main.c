bca639c1f50599863514cb3333c462d4a299caa9
Yangjiaxi <1468764755@qq.com>
2019-02-03 23:05:08 +0800

    <MOD> format

--- Chapter_15/MatrixChain/main.c
+++ Chapter_15/MatrixChain/main.c
@@ -2,7 +2,6 @@
 
 #define N 6
 
-
 /*
  * m[i, j] =
  * 0                                                if    i == j
@@ -31,9 +30,9 @@ int main()
     // shape of matrix, for matrix A_i, 1 <= i <= n
     // size is (p_{i-1}, p_i), e.g.shape(A_1) = (30, 35)
 
-    //int p[N + 1] = {30, 35, 15, 5, 10, 20, 25};   // N = 6
-    //int p[N + 1] = {10, 100, 5, 50};              // N = 3
-    int p[N + 1] = {5, 10, 3, 12, 5, 50, 6};      // N = 6
+    // int p[N + 1] = {30, 35, 15, 5, 10, 20, 25};   // N = 6
+    // int p[N + 1] = {10, 100, 5, 50};              // N = 3
+    int p[N + 1] = {5, 10, 3, 12, 5, 50, 6};  // N = 6
 
     // m[i, j] keeps the ans of A_i...A_j
     int m[N + 1][N + 1];
@@ -44,19 +43,19 @@ int main()
     {
         m[i][i] = 0;
     }
-    for (int l = 2; l <= N; l++) // length of matrix chain
+    for (int l = 2; l <= N; l++)  // length of matrix chain
     {
-        for (int i = 1; i <= N - l + 1; i++) // start point i
+        for (int i = 1; i <= N - l + 1; i++)  // start point i
         {
-            int j = i + l - 1; // end point j
-            m[i][j] = 0x3f3f3f3f;   // +inf
-            for (int k = i; k <= j - 1; k++) // split point k, i<=k<j
+            int j = i + l - 1;                // end point j
+            m[i][j] = 0x3f3f3f3f;             // +inf
+            for (int k = i; k <= j - 1; k++)  // split point k, i<=k<j
             {
-                int t = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j]; // dp
+                int t = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];  // dp
                 if (m[i][j] > t)
                 {
                     m[i][j] = t;
-                    s[i][j] = k; // record solution
+                    s[i][j] = k;  // record solution
                 }
             }
         }
@@ -78,7 +77,7 @@ int main()
     printf("Lowest Cost: %d\n", m[1][N]);
 
     printf("Solution:\n");
-    print_matrix_ans(s, 1, N); // print solution
+    print_matrix_ans(s, 1, N);  // print solution
 
     return 0;
 }
