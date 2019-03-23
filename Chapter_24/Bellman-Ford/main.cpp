3a6df493fbc34835f5cf354830303ca16a7f1b00
Yangjiaxi <1468764755@qq.com>
2019-03-23 15:45:37 +0800

    <ADD> finish bellman-ford

---
+++ Chapter_24/Bellman-Ford/main.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <iostream>
+#include "Graph/graph.h"
+
+int main()
+{
+    std::ifstream in("graph.txt");
+    Graph<char> G;
+    char u, v;
+    int w;
+    while (in >> u >> v >> w)
+    {
+        G.add(u, v, w);
+    }
+    in.close();
+    char start = 's';
+    std::vector<char> query_list{'s', 't', 'x', 'y', 'z'};
+
+    G.output();
+    std::cout << "------------------------------------" << std::endl;
+    if (G.bellman_ford(start))
+    {
+        for (char v : query_list)
+        {
+            std::cout << "From [" << start << "] to [" << v << "]" << std::endl;
+            std::cout << "\tdis: " << G.nodes[v].d << std::endl;
+            std::cout << "\tpath: ";
+            auto path = G.single_source_path(v);
+            for (int i = 0; i < path.size() - 1; ++i)
+                std::cout << path[i] << " -> ";
+            std::cout << path[path.size() - 1] << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "Negative Loop detected!" << std::endl;
+    }
+
+    return 0;
+}
